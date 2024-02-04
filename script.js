80654ee11ea5d27ae1a43311e5900d303f6fd69b
Mara Almeida <62165220+silmara502@users.noreply.github.com>
2024-02-04 20:52:38 -0300

    Animação ao Scroll

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+function initTabNav(){
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
+if(tabMenu.length && tabContent.length){
 function activeTab(index){
 
      tabContent.forEach((section) => {
@@ -15,4 +18,27 @@ tabMenu.forEach((itemMenu, index) =>{
     itemMenu.addEventListener('click', function(){
         activeTab(index);
     })
-})
+})}
+}
+initTabNav();
+function initAnimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll');
+    
+    if(sections.length){
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll(){
+       sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top;
+           const isSectionVisible = (sectionTop - windowMetade) < 0;;
+            if (isSectionVisible){
+               section.classList.add('ativo');
+            }else{
+                section.classList.remove('ativo');
+            }
+       })
+    }
+    animaScroll();
+    
+    window.addEventListener('scroll', animaScroll);
+    }}
+    initAnimacaoScroll();
